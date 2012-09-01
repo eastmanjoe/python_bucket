199550f3a2429553f89304c958502fe057343c6d
Joe Eastman <eastman.joseph@gmail.com>
2012-08-31 21:35:51 -0400

    adding programming the hard way execises.

---
+++ programming_the_hard_way/ex3.py
@@ -0,0 +1,31 @@
+# print 
+print "I will now count my chickens:"
+
+# thirty divide by six and add to five then print to screen
+print "Hens", 25.0 + 30.0 / 6
+# multiply 25 times 3 then modulo 4 and subtract from 100
+print "Roosters", 100.0 - (25.0 * 3.0) % 4.0
+
+print "Now I will count the eggs:"
+
+print (3 + 2 + 1 - 5) + (4 % 2) - (1.0 / 4) + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+# is 3 + 2 less that 5 - 7
+print (3 + 2) < (5 - 7)
+
+# calculate 3 + 2 and display result
+print "What is 3 + 2?", 3 + 2
+# calculate 5 - 7 and display result
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+# compare if 5 is greater than -2, display is true or false
+print "Is it greater?", 5 > -2
+# compare if 5 is greater than or equal to -2, display is true or false
+print "Is it greater or equal?", 5 >= -2
+# compare if 5 is less than or equal -2, display is true or false
+print "Is it less or equal?", 5 <= -2
