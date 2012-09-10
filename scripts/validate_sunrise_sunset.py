c34fe8e9b97e1917b574b6136972508c1794ab5c
Joe Eastman <joe.eastman@drakerenergy.com>
2012-09-10 15:54:27 -0400

    parse_loggernet_dat_file is successfully importing a dat file into two arrays, header and data.

--- scripts/validate_sunrise_sunset.py
+++ scripts/validate_sunrise_sunset.py
@@ -1,10 +1,16 @@
-import sys
 import solar
 import datetime
 
-latitude = 
-longitude = 
-altitude = 
+latitude = 44.467806
+longitude = -73.215565
+altitude = 34
 
-'''Import the Dat file to be  validated'''
-input_file = open(sys.argv[1], "r")
+
+'''Import the Dat file'''
+file_directory = '/home/jeastman/Programming/Python/sample_data/'
+file_name = 'project_03_panelControl.dat'
+
+file_id = open(file_directory + file_name, "r")
+
+for line in file_id:
+    print line,
