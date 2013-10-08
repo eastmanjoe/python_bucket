3d2ca53af38c3754cb4c8f07776c03829bc758d5
Joe Eastman <joe.eastman@drakerlabs.com>
2013-10-08 15:45:37 -0400

    updated script to handle multiple ports with multiple slaves.

--- scripts/modbus_slave_modbus-tk.py
+++ scripts/modbus_slave_modbus-tk.py
@@ -1,75 +1,164 @@
-#!/usr/bin/env python
+#
+#  requirement 1: serial library must be installed in the system
+#  requirement 2: modbus-tk  can be downloaded from the link below,
+#  http://code.google.com/p/modbus-tk/downloads/detail?name=modbus-tk-0.4.2.zip
+#  once downloaded it must be installed
+#
+#  This python script expects 3 STRING arguments passed-in as inputs:
+#  1) Number of modbuses in use    e.g     1  or  2
+#  2) COM PORT 'A'    e.g.    COM24   as recognized by the system
+#  3) COM PORT 'B'    e.g.    COM21   as recognized by the system
+#
+#  How to call the script from the command line example,
+#  C:\python> modbus_slave_DAS4_prod_test.py 1 COM24 COM21 True or
+#  C:\python> modbus_slave_DAS4_prod_test.py 2 COM24 COM21 False or
+#
+'''
+This program starts two modbus slaves for the DAQ code to query.
+'''
 
-#Modbus Slave using modbus-tk
-
+#import packages
+#import easygui
+import time
+import os
 import sys
-
-#add logging capability
-import logging
-import threading
-
 import serial
+import serial.tools.list_ports
+import pdb
+import argparse
+
+#---------------------------------------------------------------------------#
+# modbus-tk
 import modbus_tk
 import modbus_tk.defines as cst
 import modbus_tk.modbus_rtu as modbus_rtu
+import threading
+#---------------------------------------------------------------------------#
 
-logger = modbus_tk.utils.create_logger(name="console", record_format="%(message)s")
+#---------------------------------------------------------------------------#
+# logging
+import logging
+#---------------------------------------------------------------------------#
 
-if __name__ == "__main__":
+#---------------------------------------------------------------------------#
+def parseARGS(argv):
+    # sys.stdout.write('argv is: %s\n\r' % (argv))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('num', help='number of modbus slave ports and number of slaves per port (format is 1:1)')
+    parser.add_argument('ports', help='ports to use for the modbus slaves (format is COM1:COM2:COM3)')
+    parser.add_argument('-v','--verbose', help='enables/disable modbus-tk verbose mode', default='False')
+    parser.add_argument('-l','--level', help='defines the log level to be dispayed to the screen', default='info')
+    parser.add_argument('-f','--filename', help='defines the filename of the debugs log', default='')
+    args = parser.parse_args()
+    # sys.stdout.write('args is: %s\n\r' % (args))
+
+    return (args.num, args.ports, args.verbose, args.level, args.filename)
+#---------------------------------------------------------------------------#
+
+
+#---------------------------------------------------------------------------#
+def setupLogger(loglevel, logfilename):
+    # assuming loglevel is bound to the string value obtained from the
+    # command line argument. Convert to upper case to allow the user to
+    # specify --log=DEBUG or --log=debug
+    numeric_level = getattr(logging, loglevel.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % loglevel)
+
+    # configure logger
+    # logger.setLevel(loglevel.upper())
+    # handler_stream_formatter = logging.Formatter('%(levelname)s: %(message)s')
+    # handler_stream = logging.StreamHandler()
+    # handler_stream.setFormatter(handler_stream_formatter)
+    # handler_stream.setLevel(loglevel.upper())
+    # logger.addHandler(handler_stream)
+
+    if logfilename != '':
+        handler_file = logging.FileHandler('c:\\temp\\' + logfilename)
+        # handler_file = logging.FileHandler('/home/jeastman/logs/' + logfilename)
+        handler_file_formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
+        handler_file.setFormatter(handler_file_formatter)
+        handler_file.setLevel(loglevel.upper())
+        logger.addHandler(handler_file)
+
+    logger.info('Log Level is: %s' % (loglevel))
+    logger.info('Log Filename is: %s' % (logfilename))
+#---------------------------------------------------------------------------#
+
+
+#---------------------------------------------------------------------------#
+def startModbusSlave(modbus_ports_and_slaves_per_port, COM_PORTS, verbose_enabled):
+
+    num_modbus_ports = int(modbus_ports_and_slaves_per_port[0])
+    num_slaves_per_port = int(modbus_ports_and_slaves_per_port[1])
+
+    # modbus-tk
+    logger.info('Number of Modbus Slave Ports is %d' % num_modbus_ports)
+    logger.info('Number of Modbus Slaves per Port is %d' % num_slaves_per_port)
+
+    for x in xrange(0, num_modbus_ports):
+        logger.info('COM Port #%d is: %s' % (x + 1, COM_PORTS[x]))
+
     try:
+        for modbus_ports in xrange(0, num_modbus_ports):
+            exec('server_%d = modbus_rtu.RtuServer(serial.Serial(port="%s", baudrate=9600, bytesize=8, parity="N", stopbits=1, xonxoff=0))' % (modbus_ports, str(COM_PORTS[modbus_ports])))
 
-        server = modbus_rtu.RtuServer(serial.Serial(port=4, baudrate=9600, bytesize=8, parity='N', stopbits=1, xonxoff=0))
+            if verbose_enabled == 'True':
+                logger.info('Modbus-tk verbose enabled')
+                exec("server_%d.set_verbose(True)" % (modbus_ports))
+            else:
+                logger.info('Modbus-tk verbose disabled')
+                exec("server_%d.set_verbose(False)" % (modbus_ports))
+
+
+            exec('server_%d.start()' % (modbus_ports))
+
+            for modbus_slaves in xrange(0,num_slaves_per_port):
+                exec("server_%d_slave_%d = server_%d.add_slave(11 + %d)" % (modbus_ports, modbus_slaves, modbus_ports, modbus_slaves))
+                exec("server_%d_slave_%d.add_block('0', cst.HOLDING_REGISTERS, 100, 10)" % (modbus_ports, modbus_slaves))
+
+                holding_register_values = range(10 + (10 * modbus_slaves), 20 + (10 * modbus_slaves))
+
+                exec("server_%d_slave_%d.set_values('0', 100, [%s])" % \
+                    (modbus_ports, modbus_slaves, ','.join(map(str, holding_register_values))))
 
         #Connect to the slave
-        logger.info("running...")
-        logger.info("enter 'quit' for closing the server")
-
-        server.start()
-
-        slave_1 = server.add_slave(11)
-        slave_1.add_block('0', cst.HOLDING_REGISTERS, 0, 100)
-        slave_1.set_values('0', 0, [1,2,3])
+        logger.info('All slave(s) running...')
+        logger.info('enter "quit" to close the server(s)')
 
         while True:
             cmd = sys.stdin.readline()
             args = cmd.split(' ')
-            if cmd.find('quit')==0:
-                sys.stdout.write('bye-bye\r\n')
+
+            if ((cmd.find('quit')==0) or (cmd.find('q')==0)):
+                sys.stdout.write('bye-bye\n')
                 break
-            elif args[0]=='add_slave':
-                slave_id = int(args[1])
-                server.add_slave(slave_id)
-                sys.stdout.write('done: slave %d added\r\n' % (slave_id))
-            elif args[0]=='add_block':
-                slave_id = int(args[1])
-                name = args[2]
-                block_type = int(args[3])
-                starting_address = int(args[4])
-                length = int(args[5])
-                slave = server.get_slave(slave_id)
-                slave.add_block(name, block_type, starting_address, length)
-                sys.stdout.write('done: block %s added\r\n' % (name))
-            elif args[0]=='set_values':
-                slave_id = int(args[1])
-                name = args[2]
-                address = int(args[3])
-                values = []
-                for v in args[4:]:
-                    values.append(int(v))
-                slave = server.get_slave(slave_id)
-                slave.set_values(name, address, values)
-                values = slave.get_values(name, address, len(values))
-                sys.stdout.write('done: values written: %s\r\n' % (str(values)))
-            elif args[0]=='get_values':
-                slave_id = int(args[1])
-                name = args[2]
-                address = int(args[3])
-                length = int(args[4])
-                slave = server.get_slave(slave_id)
-                values = slave.get_values(name, address, length)
-                sys.stdout.write('done: values read: %s\r\n' % (str(values)))
-            else:
-                sys.stdout.write("unknown command %s\r\n" % (args[0]))
 
     finally:
-        server.stop()
+        for modbus_ports in xrange(0, num_modbus_ports):
+            exec('server_%d.stop()' % (modbus_ports))
+#---------------------------------------------------------------------------#
+
+
+#---------------------------------------------------------------------------#
+if __name__ == '__main__':
+
+
+    args = parseARGS(sys.argv[1:])
+
+    loglevel = str(args[3])
+
+    # start-up logger
+    logger = modbus_tk.utils.create_logger(name='console', level=loglevel.upper() ,record_format="%(levelname)s: %(message)s")
+    # logger = logging.getLogger('modbus_log')
+
+    # setupLogger('debug', 'modbus_log.log')
+    setupLogger(args[3], args[4])
+
+    logger.info('Script started on: %s' % time.asctime(time.localtime(time.time())))
+
+    startModbusSlave(args[0].split(':'), args[1].split(':'), args[2])
+
+def dependencies_for_freeezing():
+    import modbus_tk
+    import socket
