5fc585dc147b9a7de232ae25d029a7974c90b971
Joe Eastman <joe.eastman@drakerlabs.com>
2014-02-05 10:46:08 -0500

    removed unused script

--- scripts/launch_etl_testing.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/bash
-
-# This script opens a terminal and run the ETL testing script
-
-xterm -hold -e "ls; bash"
-# xterm -e "/home/jeastman/virtualenvs/pymodbus/bin/python /home/jeastman/etl_netbook_testing.py -l info -f etl_testing.log"
