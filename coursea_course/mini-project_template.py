813f697cfaeba5be52141f65d8f8ac6da964cd34
Joe Eastman <joe.eastman@drakerenergy.com>
2014-09-26 10:38:54 -0400

    adding template for coursea mini-projects.

---
+++ coursea_course/mini-project_template.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+'''
+Mini-project - Week ##
+
+Mini-project description
+
+'''
+# URL for assignment tempalte
+#
+
+# URL for completed assignement
+#
+
+# Copy and paste below the line into CodeSkulptor
+#------------------------------------------------------------------------------
+
