0c857eda0e397748bbfc131f6a70946e43358f22
Joe Eastman <eastman.joseph@gmail.com>
2014-10-02 22:00:46 -0400

    spelling correction and reformating.

--- coursea_course/mini-project_template.py
+++ coursea_course/mini-project_template.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 
+# URL for assignment template
+#
+
+# URL for completed assignment
+#
+
+# Copy and paste below the line into CodeSkulptor
+#------------------------------------------------------------------------------
+
 '''
 Mini-project - Week ##
 
 Mini-project description
 
 '''
-# URL for assignment tempalte
-#
-
-# URL for completed assignement
-#
-
-# Copy and paste below the line into CodeSkulptor
-#------------------------------------------------------------------------------
-
