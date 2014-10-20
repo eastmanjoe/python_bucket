1dc64b4fc6f59a6b9d1141a951d344bee5c85f1f
Joe Eastman <joe.eastman@drakerenergy.com>
2014-10-20 17:29:48 -0400

    added the use of python module splinter

--- scripts/query_raven.py
+++ scripts/query_raven.py
@@ -1,12 +1,31 @@
-import urllib2
+#!/usr/bin/env Python
 
-data = '<body><read><data id="0"/><data id="16384"/><data id="20000"/></read></body>'
+''' Configure Raven XE
+'''
+# import urllib2
+from splinter import Browser
 
-auth_handler = urllib2.HTTPBasicAuthHandler()
-auth_handler.add_password("Modem@AirLink.com", 'msci', 'user', '12345')
+# data = '<body><read><data id="0"/><data id="16384"/><data id="20000"/></read></body>'
 
-opener = urllib2.build_opener(auth_handler)
+# auth_handler = urllib2.HTTPBasicAuthHandler()
+# auth_handler.add_password("Modem@AirLink.com", 'msci', 'user', '12345')
 
-urllib2.install_opener(opener)
+# opener = urllib2.build_opener(auth_handler)
 
-urllib2.urlopen('http://192.168.14.31:8088', data)
+# urllib2.install_opener(opener)
+
+# urllib2.urlopen('http://192.168.14.31:8088', data)
+
+browser = Browser('firefox')
+browser.visit('http://166.161.64.248:9191')
+browser.fill('password', 12345)
+browser.find_by_name('Login').click()
+# browser.find_by_id('btn_tpl').click()
+# browser.attach_file('up.upload-file', 'C:\cygwin\home\jeastman\hardware-config\CommonConfigs\RAVEN_XE_VERIZON.xml')
+# browser.find_by_name('lApply').click()
+browser.find_by_id('Cont.TopM1.SecurityM1').click()
+# browser.fill('3502', 2)
+
+
+
+browser.quit()
