6330cb7d64a9c7e3a81716b14633e8d79a2989f1
Joe Eastman <joe.eastman@drakerenergy.com>
2015-03-11 17:21:36 -0400

    removing token

--- scripts/query_gitHub.py
+++ scripts/query_gitHub.py
@@ -1,6 +1,6 @@
 import requests
 
-header = {'Authorization': 'token ded311305ca64ca0d45c649573b84f43a88da3cc'}
+header = {'Authorization': 'token'}
 url = "https://raw.githubusercontent.com/drakerlabs/project-firmware/master/projects/p2592-01_north-high-school.CR8"
 
 r = requests.get(url, headers=header)
