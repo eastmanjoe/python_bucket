df133be980374ce6d8d954341da154ad6595fbf8
Joe Eastman <joe.eastman@drakerenergy.com>
2015-04-23 10:40:45 -0400

    adding script to list installed modules

---
+++ modules/list_installed_modules.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import pip
+
+if __name__ == '__main__':
+    installed_packages = pip.get_installed_distributions()
+    installed_packages_list =
+        sorted(["%s==%s" % (i.key, i.version) for i in installed_packages])
+
+    print(installed_packages_list)
