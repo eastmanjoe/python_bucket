834b36d9236f0b4db01fa95965bca2f8ad09b6cf
Joe Eastman <joe.eastman@drakerenergy.com>
2015-07-31 09:26:54 -0400

    unknow version of the cora script.  does not currently work.  trying to start the cora instance then interact with it.

--- modules/cora.py
+++ modules/cora.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 '''
-Run Cora commands and generate an XML file with all of the settings
+Run Cora commands
 '''
 
 #---------------------------------------------------------------------------#
@@ -12,58 +12,97 @@ import argparse
 import time
 import logging
 import threading
-import subprocess
-from lxml import etree
+from subprocess import Popen, PIPE, STDOUT
+import json
 
 #---------------------------------------------------------------------------#
-def main(server_ip, username, password):
+class Cora(threading.Thread):
+    '''
+    a thread that launches the cora_cmd service and
+    connect to the LoggerNet server specifiec
+    '''
 
-    page = etree.Element('loggerNet_Network')
-    doc = etree.ElementTree(page)
+    def __init__(self, server_ip, username, password):
+        super(Cora, self).__init__()
+        self.name = 'Cora'
+        self.threadID = 2
+        self.stoprequest = threading.Event()
+        self.server_ip = server_ip
+        self.username = username
+        self.password = password
 
-    cora_cmd = "cora_cmd"
-    cora_cmd += " --input={connect " + server_ip
-    if username != "":
-        cora_cmd += " --username=" + username
-        cora_cmd += " --password=" + password
 
-    cora_cmd += ";"
+    def run(self):
+        logger.info('starting cora')
+        self.cora = "cora"
+        self.cora += " --input={connect " + server_ip
+        if username != "":
+            self.cora += " --username=" + self.username
+            self.cora += " --password=" + self.password
 
-    cora_cmd += "list-devices;"
-    cora_cmd += "}"
+        cora += ";}"
 
-    # print cora_cmd
+        self.terminal = Popen(self.cora, stdout=PIPE, stdin=PIPE, stderr=STDOUT)
 
-    # run list-device cora script command to get list of devices on server
-    # output = subprocess.Popen(cora_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    # list_of_devices, error = output.communicate()
-    list_of_devices = subprocess.check_output(cora_cmd)
+    def join(self):
+        self.stoprequest.set()
+        super(dataValidation, self).join(timeout)
 
-    # Extract the device list
-    list_of_devices = list_of_devices[list_of_devices.find("*list-devices"):list_of_devices.find("+list-devices")]
-    list_of_devices = list_of_devices.strip("*list-devices")
-    list_of_devices = list_of_devices.strip("\r\n")
-    list_of_devices = list_of_devices.split("\r\n")
+        logger.info('stopping cora')
 
-    # print list_of_devices
+    def list_stations(self):
 
-    device_list = []
+        cmd += "list-stations;"
+        cmd += "}"
 
-    for device in list_of_devices[1:len(list_of_devices) - 1]:
-        # start = device.find("{")
-        # stop = device.find("}")
-        # device = device[start + 1:len(device) - 1]
+    def network_map_xml(self):
 
-        device = device.replace("{", "")
-        device = device.replace("}", "")
-        device = device.split()
+        cmd += "make-xml-network-map --format=xml" + ";"
+        cmd += "}"
 
-        device_list.append(device)
+    def get_data_fill_days(self, station_name):
+        cmd += "get-value " + station_name + "Status.DataFillDays(1);"
+        cmd += "}"
 
-    print device_list
+    def list_devices(self):
+        '''
+        get a list of the devices on the loggernet server
+        '''
 
-    outFile = open("LoggerNet_" + server_ip + "_Settings.xml", "w")
-    doc.write(outFile)
+        cmd = "list-devices;}"
+
+        # print cora_cmd
+
+        # run list-device cora script command to get list of devices on server
+        # output = subprocess.Popen(cora_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        # list_of_devices, error = output.communicate()
+        list_of_devices = self.terminal.communicate(cora_cmd + cmd)
+
+        # Extract the device list
+        list_of_devices = list_of_devices[list_of_devices.find("*list-devices"):list_of_devices.find("+list-devices")]
+        list_of_devices = list_of_devices.strip("*list-devices")
+        list_of_devices = list_of_devices.strip("\r\n")
+        list_of_devices = list_of_devices.split("\r\n")
+
+        # print list_of_devices
+
+        device_list = []
+
+        for device in list_of_devices[1:len(list_of_devices) - 1]:
+            # start = device.find("{")
+            # stop = device.find("}")
+            # device = device[start + 1:len(device) - 1]
+
+            device = device.replace("{", "")
+            device = device.replace("}", "")
+            device = device.split()
+
+            device_list.append(device)
+
+        print device_list
+
+        outFile = open("LoggerNet_" + self.server_ip + "_Settings.xml", "w")
+        doc.write(outFile)
 
 
 #---------------------------------------------------------------------------#
@@ -75,4 +114,4 @@ if __name__ == "__main__":
     parser.add_argument('--password', help='password for LoggerNet server', default="")
     args = parser.parse_args()
 
-    main(args.server_ip, args.username, args.password)
+    list_devices(args.server_ip, args.username, args.password)
