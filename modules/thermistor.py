4a278ffee2937d3fd181864e0fe648268be05080
Joe Eastman <joe.eastman@drakerenergy.com>
2017-04-19 08:45:08 -0400

    removed Stein-Hart calc from thermistor class

--- modules/thermistor.py
+++ modules/thermistor.py
@@ -7,6 +7,46 @@
 # import modules
 import math
 
+
+def calcSteinhartHartConstants(data):
+    """
+    Calculates the Steinhart-Hart Equation constants based upon 3 temperature/resistance test points.
+    The temperature is in Celcius and the resistance is in ohms.
+    The function arguments are contained within a dictionary.
+    The Steinhart-Hart constants are returned in a dictionary.
+
+    Example:
+    thermistor_data = { 'temp_c_low': 10, 'temp_r_low': 58750,
+                        'temp_c_mid': 25, 'temp_r_mid': 30000,
+                        'temp_c_high': 40, 'temp_r_high': 16150}
+
+    thermistor_steinhart_constants = calcSteinhartHartConstants(thermistor_data)
+    """
+
+    data['temp_k_low'] = data['temp_c_low'] + 273.15
+    data['temp_k_mid'] = data['temp_c_mid'] + 273.15
+    data['temp_k_high'] = data['temp_c_high'] + 273.15
+
+    diff_log_low_med = math.log(data['temp_r_low']) - math.log(data['temp_r_mid'])
+    diff_log_low_high = math.log(data['temp_r_low']) - math.log(data['temp_r_high'])
+    diff_temp_low_mid = (1 / data['temp_k_low']) - (1 / data['temp_k_mid'])
+    diff_temp_low_high = (1 / data['temp_k_low']) - (1 / data['temp_k_high'])
+
+    # print diff_temp_low_mid, diff_temp_low_high
+    # print diff_log_low_med, diff_log_low_high
+
+    sh_c = ((diff_temp_low_mid - (diff_log_low_med * (diff_temp_low_high/diff_log_low_high))) /
+            ((math.log(data['temp_r_low'])**3 - math.log(data['temp_r_mid'])**3) -
+            (diff_log_low_med * (math.log(data['temp_r_low'])**3 - math.log(data['temp_r_high'])**3) / diff_log_low_high)))
+
+    sh_b = (diff_temp_low_mid - (sh_c * (math.log(data['temp_r_low'])**3 - math.log(data['temp_r_mid'])**3))) / diff_log_low_med
+
+    sh_a = (1 / data['temp_k_low']) - (sh_c * math.log(data['temp_r_low'])**3) - (sh_b * math.log(data['temp_r_low']))
+
+    # print "A = ", self.sh_a, "; B =", self.sh_b,"; C =", self.sh_c
+    return {'sh_a': sh_a, 'sh_b': sh_b, 'sh_c': sh_c}
+
+
 class thermistor:
     """useful calculations for thermistors."""
     def __init__(self, sh_a, sh_b, sh_c):
@@ -14,49 +54,6 @@ class thermistor:
         self.sh_a = sh_a
         self.sh_b = sh_b
         self.sh_c = sh_c
-
-
-    def calcSteinhartHartConstants(self, data):
-        """
-        Calculates the Steinhart-Hart Equation constants based upon 3 temperature/resistance test points.
-        The temperature is in Celcius and the resistance is in ohms.
-        The function arguments are contained within a dictionary.
-        The Steinhart-Hart constants are returned in a dictionary.
-
-        Example:
-        thermistor_data = { 'temp_c_low': 10, 'temp_r_low': 58750,
-                            'temp_c_mid': 25, 'temp_r_mid': 30000,
-                            'temp_c_high': 40, 'temp_r_high': 16150}
-
-        t1 = Thermistor(9.376e-4, 2.208e-4, 1.276e-7)
-        thermistor_steinhart_constants = t1.calcSteinhartHartConstants(thermistor_data)
-        """
-
-        data['temp_k_low'] = data['temp_c_low'] + 273.15
-        data['temp_k_mid'] = data['temp_c_mid'] + 273.15
-        data['temp_k_high'] = data['temp_c_high'] + 273.15
-
-        diff_log_low_med = math.log(data['temp_r_low']) - math.log(data['temp_r_mid'])
-        diff_log_low_high = math.log(data['temp_r_low']) - math.log(data['temp_r_high'])
-        diff_temp_low_mid = (1 / data['temp_k_low']) - (1 / data['temp_k_mid'])
-        diff_temp_low_high = (1 / data['temp_k_low']) - (1 / data['temp_k_high'])
-
-        # print diff_temp_low_mid, diff_temp_low_high
-        # print diff_log_low_med, diff_log_low_high
-
-        self.sh_c = ((diff_temp_low_mid - (diff_log_low_med * (diff_temp_low_high/diff_log_low_high))) /
-                ((math.log(data['temp_r_low'])**3 - math.log(data['temp_r_mid'])**3) -
-                (diff_log_low_med * (math.log(data['temp_r_low'])**3 - math.log(data['temp_r_high'])**3) / diff_log_low_high)))
-
-        self.sh_b = (diff_temp_low_mid - (self.sh_c * (math.log(data['temp_r_low'])**3 - math.log(data['temp_r_mid'])**3))) / diff_log_low_med
-
-        self.sh_a = (1 / data['temp_k_low']) - (self.sh_c * math.log(data['temp_r_low'])**3) - (self.sh_b * math.log(data['temp_r_low']))
-
-        # print "A = ", self.sh_a, "; B =", self.sh_b,"; C =", self.sh_c
-        return {'sh_a': self.sh_a, 'sh_b': self.sh_b, 'sh_c': self.sh_c}
-
-
-
 
     def calcTemp(self, resistance):
         """
@@ -115,33 +112,49 @@ if __name__ == '__main__':
 
     # Omega 44008
     # t1 = thermistor(9.376e-4, 2.208e-4, 1.276e-7)
-    # t2 = thermistor(.0009376, .0002208, .0000001276)
-
-    # CSI 109
-    t3 = thermistor(1.129241e-3, 2.341077e-4, 8.775468e-8)
-    # thermistor_data = { 'temp_c_low': -40, 'temp_r_low': 884600,
-    #                     'temp_c_mid': 25, 'temp_r_mid': 30000,
-    #                     'temp_c_high': 150, 'temp_r_high': 550200}
-
 
     # A = 9.26e-04, B = 2.22e-04, C = 1.23e-07
-    # thermistor_data = { 'temp_c_low': 10, 'temp_r_low': 58750,
-    #                     'temp_c_mid': 25, 'temp_r_mid': 30000,
-    #                     'temp_c_high': 40, 'temp_r_high': 16150}
-
-    # thermistor_steinhart_constants = t1.calcSteinhartHartConstants(thermistor_data)
-
-    # print 'The Steinhart-Hart constants are: ', thermistor_steinhart_constants
+    # thermistor_data = dict(temp_c_low=10, temp_r_low=58750, temp_c_mid=25, temp_r_mid=30000, temp_c_high=40,
+    #                        temp_r_high=16150)
+    #
+    # thermistor_steinhart_constants = calcSteinhartHartConstants(thermistor_data)
+    # print 'The Omega 44008 Steinhart-Hart constants are: ', thermistor_steinhart_constants
 
     # calc_temp = t1.calcTemp(30000)
-    # calc_temp = t2.calcTemp(30000)
-    calc_temp = t3.calcTemp(43718.22)
-    print 'Temperature for Omega Thermistor @ 30kOhms is 25 Deg C, Calculated Temp is:', calc_temp
+    # print 'Temperature for Omega Thermistor @ 30kOhms is 25 Deg C, Calculated Temp is:', calc_temp
 
     # calc_res = t1.calcResistance(25)
-    # calc_res = t2.calcResistance(25)
     # print 'Resistance for Omega Thermistor @ 25 Deg C is 30k, Calculated Res is:', calc_res
 
     # brige_temp = t1.calcThermistorBridge(2.31858, 5, 24900)
+    # print 'Expected Temperature: 32.91281, Calculated Temperature is:', brige_temp
+
+    # t2 = thermistor(.0009376, .0002208, .0000001276)
+
+    # calc_temp = t2.calcTemp(30000)
+    # print 'Temperature for Omega Thermistor @ 30kOhms is 25 Deg C, Calculated Temp is:', calc_temp
+
+    # calc_res = t2.calcResistance(25)
+    # print 'Resistance for Omega Thermistor @ 25 Deg C is 30k, Calculated Res is:', calc_res
+
     # brige_temp = t2.calcThermistorBridge(2.31858, 5, 24900)
     # print 'Expected Temperature: 32.91281, Calculated Temperature is:', brige_temp
+    # CSI 109
+    # t3 = thermistor(1.129241e-3, 2.341077e-4, 8.775468e-8)
+    # thermistor_data = dict(temp_c_low=-40, temp_r_low=884600, temp_c_mid=25, temp_r_mid=30000, temp_c_high=150,
+    #                        temp_r_high=550200)
+
+    # calc_temp = t3.calcTemp(43718.22)
+    # print 'Temperature for CSI 109 Thermistor @ 30kOhms is 25 Deg C, Calculated Temp is:', calc_temp
+
+    thermistor_data = dict(temp_c_low=0, temp_r_low=97950.00, temp_c_mid=25, temp_r_mid=30000.00, temp_c_high=50,
+                           temp_r_high=10806.00)
+
+    thermistor_steinhart_constants = calcSteinhartHartConstants(thermistor_data)
+    print 'The US Sensor PS303J2 Steinhart-Hart constants are: ', thermistor_steinhart_constants
+
+    thermistor_data = dict(temp_c_low=0, temp_r_low=95280.00, temp_c_mid=25, temp_r_mid=30000.00, temp_c_high=50,
+                           temp_r_high=10971.00)
+
+    thermistor_steinhart_constants = calcSteinhartHartConstants(thermistor_data)
+    print 'The Amphenol EC95H303W Steinhart-Hart constants are: ', thermistor_steinhart_constants
