04f1b9e0eca892bff67c0f136518635621534a10
Joe Eastman <joe.eastman@drakerenergy.com>
2017-05-26 14:38:09 -0400

    fix out of range error

--- scripts/modbus_slave_modbus-tk.py
+++ scripts/modbus_slave_modbus-tk.py
@@ -159,7 +159,7 @@ def start_modbus_slave(modbus_ports_and_slaves_per_port, com_ports, verbose_enab
                 logger.debug(
                     'Added values {} to {} to modbus slave #{}'.format(
                         mb_slave.get_values('0', 1, 1)[0],
-                        mb_slave.get_values('0', holding_register_values, 1)[0],
+                        mb_slave.get_values('0', number_of_registers, 1)[0],
                         1 + modbus_slaves
                     )
                 )
