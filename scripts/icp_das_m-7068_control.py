1549d602f26263825950e9a0a11b468a09bea017
Joe Eastman <joe.eastman@drakerenergy.com>
2017-09-29 10:28:41 -0400

    initial script to test control of an icp das m-7068

---
+++ scripts/icp_das_m-7068_control.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+
+#Modbus Master using modbus-tk
+
+#import sys
+import serial
+import modbus_tk
+import modbus_tk.defines as cst
+import modbus_tk.modbus_rtu as modbus_rtu
+import modbus_tk.modbus_tcp as modbus_tcp
+import logging
+import argparse
+from time import sleep
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--level', help='defines the log level to be dispayed to the screen', default='info')
+    parser.add_argument('-f', '--filename', help='defines the filename of the debugs log', default='')
+    parser.add_argument('-ip', '--ipaddress', help='defines the ip address to query', default='')
+    parser.add_argument('-p', '--port', help='set port of modbus slave', default='/dev/ttyUSB0')
+    parser.add_argument('-mb', '--modbus_address', help='set Modbus Address', default='1')
+    args = parser.parse_args()
+
+    # assuming args.level is bound to the string value obtained from the
+    # command line argument. Convert to upper case to allow the user to
+    # specify --log=DEBUG or --log=debug
+    numeric_level = getattr(logging, args.level.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % args.level)
+
+    logger = modbus_tk.utils.create_logger(name="console", level=args.level.upper(), record_format="%(levelname)s: %(message)s")
+    # logger = logging.getLogger('modbus_tk_tcp_slave')
+    # # configure logger
+    # # logger.setLevel(logging.DEBUG)
+    # handler_stream_formatter = logging.Formatter('%(levelname)s: %(message)s')
+    # handler_stream = logging.StreamHandler()
+    # handler_stream.setFormatter(handler_stream_formatter)
+    # handler_stream.setLevel(args.level.upper())
+    # logger.addHandler(handler_stream)
+    logger.debug('Log Level is: %s' % (args.level.upper()))
+
+    # if args.filename != '':
+    #     handler_file = logging.FileHandler('/home/jeastman/logs/' + args.filename)
+    #     logger.debug('Log Filename is: %s' % (handler_file))
+    #     handler_file_formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
+    #     handler_file.setFormatter(handler_file_formatter)
+    #     # handler_file.setLevel(level.DEBUG)
+    #     logger.addHandler(handler_file)
+    # logger.debug('Log Filename is: %s' % (args.filename))
+
+    try:
+        reg_hex = []
+
+        if args.port.isdigit():
+            # if args.ipaddress.split('.') :
+                # raise ValueError('Invalid IP Address')
+            #Connect to the TCP slave
+            print "Modbus TCP"
+            master = modbus_tcp.TcpMaster(host=args.ipaddress, port=int(args.port), timeout_in_sec=1.0)
+        else:
+            print "Modbus RTU"
+            # connect to the RTU slave
+            master = modbus_rtu.RtuMaster(serial.Serial(port=args.port, baudrate=9600, bytesize=8, parity='N', stopbits=1, xonxoff=0))
+        master.set_timeout(1.0)
+        master.set_verbose(True)
+        logger.info("connected")
+
+        # Serial Number
+        regs = master.execute(int(args.modbus_address), cst.WRITE_MULTIPLE_COILS, 7, 0)
+        print regs
+
+        sleep(1)
+
+        regs = master.execute(int(args.modbus_address), cst.WRITE_MULTIPLE_COILS, 7, 1)
+        print regs
+
+        sleep(1)
+
+        regs = master.execute(int(args.modbus_address), cst.WRITE_MULTIPLE_COILS, 7, 3)
+        print regs
+
+        sleep(1)
+
+        regs = master.execute(int(args.modbus_address), cst.WRITE_MULTIPLE_COILS, 7, 7)
+        print regs
+
+        sleep(1)
+
+        regs = master.execute(int(args.modbus_address), cst.WRITE_MULTIPLE_COILS, 7, 135)
+        print regs
+
+        sleep(1)
+
+        regs = master.execute(int(args.modbus_address), cst.WRITE_MULTIPLE_COILS, 7, 134)
+        print regs
+
+        sleep(1)
+
+        regs = master.execute(int(args.modbus_address), cst.WRITE_MULTIPLE_COILS, 7, 132)
+        print regs
+
+        sleep(1)
+
+        regs = master.execute(int(args.modbus_address), cst.WRITE_MULTIPLE_COILS, 7, 128)
+        print regs
+
+        sleep(1)
+
+        regs = master.execute(int(args.modbus_address), cst.WRITE_MULTIPLE_COILS, 7, 0)
+        print regs
+
+        master.close()
+
+    except modbus_tk.modbus.ModbusError, e:
+        logger.error("%s- Code=%d" % (e, e.get_exception_code()))
